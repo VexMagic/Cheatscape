9716de53f0baeb0b739782eed4ea498adf323569
VexMagic <VeraLynnTeahan@outlook.com>
2022-04-03 23:02:47 +0200

    Added so the starting positions of all the pieces is correct

--- Cheatscape/Game Board.cs
+++ Cheatscape/Game Board.cs
@@ -18,23 +18,54 @@ namespace Cheatscape
         {
             ChessBoard = Global_Info.AccessContentManager.Load<Texture2D>("Chess Board");
 
-            int tempPiece = 0;
-            bool tempOwner = true;
-
-            for (int i = 0; i < ChessPiecesOnBoard.GetLength(0); i++)
+            for (int x = 0; x < ChessPiecesOnBoard.GetLength(0); x++)
             {
-                for (int j = 0; j < ChessPiecesOnBoard.GetLength(1); j++)
+                for (int y = 0; y < ChessPiecesOnBoard.GetLength(1); y++)
                 {
-                    ChessPiecesOnBoard[i, j] = new Chess_Piece(tempPiece, tempOwner);
-                    tempPiece++;
-                    if (tempPiece >= 7)
-                    {
-                        tempPiece = 0;
-                    }
-                    if (tempOwner)
-                        tempOwner = false;
+                    int tempPiece = 0;
+                    bool tempIsWhite = true;
+
+                    if (y <= 3)
+                        tempIsWhite = false;
                     else
-                        tempOwner = true;
+                        tempIsWhite = true;
+
+                    switch (y)
+                    {
+                        case 0:
+                        case 7:
+                            switch (x)
+                            {
+                                case 0:
+                                case 7:
+                                    tempPiece = 2;
+                                    break;
+                                case 1:
+                                case 6:
+                                    tempPiece = 4;
+                                    break;
+                                case 2:
+                                case 5:
+                                    tempPiece = 3;
+                                    break;
+                                case 3:
+                                    tempPiece = 5;
+                                    break;
+                                case 4:
+                                    tempPiece = 6;
+                                    break;
+                            }
+                            break;
+                        case 1:
+                        case 6:
+                            tempPiece = 1;
+                            break;
+                        default:
+                            tempPiece = 0;
+                            break;
+                    }
+
+                    ChessPiecesOnBoard[x, y] = new Chess_Piece(tempPiece, tempIsWhite);
                 }
             }
         }
@@ -49,12 +80,12 @@ namespace Cheatscape
         {
             aSpriteBatch.Draw(ChessBoard, BoardPosition, Color.White);
 
-            for (int i = 0; i < ChessPiecesOnBoard.GetLength(0); i++)
+            for (int x = 0; x < ChessPiecesOnBoard.GetLength(0); x++)
             {
-                for (int j = 0; j < ChessPiecesOnBoard.GetLength(1); j++)
+                for (int y = 0; y < ChessPiecesOnBoard.GetLength(1); y++)
                 {
-                    Vector2 tempPiecePos = new Vector2(BoardPosition.X + (i * TileSize), BoardPosition.Y + (j * TileSize));
-                    ChessPiecesOnBoard[i, j].Draw(aSpriteBatch, tempPiecePos);
+                    Vector2 tempPiecePos = new Vector2(BoardPosition.X + (x * TileSize), BoardPosition.Y + (y * TileSize));
+                    ChessPiecesOnBoard[x, y].Draw(aSpriteBatch, tempPiecePos);
                 }
             }
         }
