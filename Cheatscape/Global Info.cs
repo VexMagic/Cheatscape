ee0fad92db98d18c4afc9b11e7c3845376fe0d3e
BobbanW <89905154+BobbanW@users.noreply.github.com>
2022-04-28 13:29:08 +0200

    Added a main menu

--- Cheatscape/Global Info.cs
+++ Cheatscape/Global Info.cs
@@ -13,8 +13,8 @@ namespace Cheatscape
         static float ScreenScale = 2f;
         public static Vector2 WindowSize = new Vector2(600 * ScreenScale, 360 * ScreenScale);
         static int ButtonCooldown = 0;
-        public enum GameState { LevelSelect, PlayingLevel };
-        static GameState CurrentGameState = GameState.LevelSelect;
+        public enum GameState { LevelSelect, PlayingLevel, MainMenu };
+        static GameState CurrentGameState = GameState.MainMenu;
 
         public static ContentManager AccessContentManager { get => ContentManager; set => ContentManager = value; }
         public static float AccessScreenScale { get => ScreenScale; set => ScreenScale = value; }
@@ -27,13 +27,14 @@ namespace Cheatscape
         {
             Game_Board.Load();
             //File_Manager.LoadLevel();
+            Main_Menu.Load();
             Hand_Animation_Manager.Load();
             Level_Select_Menu.Load();
             Rules_List.Load();
             Text_Manager.Load();
         }
 
-        public static void Update()
+        public static void Update(GameTime gameTime)
         {
             if (ButtonCooldown > 0)
                 ButtonCooldown--;
@@ -47,6 +48,9 @@ namespace Cheatscape
                     Level_Manager.Update();
                     Hand_Animation_Manager.Update();
                     break;
+                case GameState.MainMenu:
+                    Main_Menu.Update(gameTime);
+                    break;
             }
         }
 
@@ -55,6 +59,7 @@ namespace Cheatscape
             switch (CurrentGameState)
             {
                 case GameState.LevelSelect:
+                    Main_Menu.Draw(aSpriteBatch);
                     Level_Select_Menu.Draw(aSpriteBatch);
                     break;
                 case GameState.PlayingLevel:
@@ -62,6 +67,9 @@ namespace Cheatscape
                     Hand_Animation_Manager.Draw(aSpriteBatch);
                     Level_Manager.Draw(aSpriteBatch);
                     Text_Manager.DrawTutorialBox(aSpriteBatch);
+                    break;
+                case GameState.MainMenu:
+                    Main_Menu.Draw(aSpriteBatch);
                     break;
             }
         }
---
+++ Cheatscape/Main Menu.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Cheatscape
+{
+    static class Main_Menu
+    {
+
+        static int CurrentFrame = 0;
+        const int MAXFRAME = 8;
+        static bool animating = false;
+        static int frame = 1;
+        static float timer;
+        static float framesPerSecond = 151f;
+        static Texture2D menuScreen;
+        static Rectangle srcRect;
+        static int width = 1920;
+        static int height = 1080;
+
+        public static void Load()
+        {
+            menuScreen = Global_Info.AccessContentManager.Load<Texture2D>("Main_Menu");
+        }
+        public static void Update(GameTime gameTime)
+        {
+            if (animating == true)
+            {
+                timer += (float)gameTime.ElapsedGameTime.Milliseconds;
+
+                if (timer >= framesPerSecond)
+                {
+                    CurrentFrame += frame;
+                    timer = 0;
+                }
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Space))
+            {
+                animating = true;
+            }
+
+            if (CurrentFrame == MAXFRAME)
+            {
+                animating = false;
+                Global_Info.AccessCurrentGameState = Global_Info.GameState.LevelSelect;
+
+            }
+
+            srcRect = new Rectangle(0, height * CurrentFrame, width, height);
+
+        }
+        public static void Draw(SpriteBatch aSpriteBatch)
+        {
+            aSpriteBatch.Draw(menuScreen, new Rectangle(0,0,(int)(Global_Info.WindowSize.X/Global_Info.AccessScreenScale), (int)(Global_Info.WindowSize.Y / Global_Info.AccessScreenScale)), srcRect, Color.White);
+        }
+    }
+}
