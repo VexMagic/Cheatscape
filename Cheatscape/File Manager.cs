a9b1a5599317303a8e1dc989bcf309074b6ad35a
Johan Körmark <Johan.kormark@gmail.com>
2022-05-22 15:37:47 +0200

    fixa transition på sista leveln i en bundle

--- Cheatscape/File Manager.cs
+++ Cheatscape/File Manager.cs
@@ -43,6 +43,7 @@ namespace Cheatscape
             catch
             {
                 Global_Tracker.AddCompletedLevel(Level_Select_Menu.SelectedBundleX, Level_Manager.AccessRating);
+                Level_Manager.isOnTransitionScreen = false;
                 End_Screen.AccessCleared = true;
                 End_Screen.AccessIsEnded = true;
             }
