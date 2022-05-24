ec58e2baefe756acaf9458980f7cef0ed0185931
Johan Körmark <Johan.kormark@gmail.com>
2022-05-24 13:07:25 +0200

    mouse controlls fix

--- Cheatscape/Level Manager.cs
+++ Cheatscape/Level Manager.cs
@@ -186,22 +186,38 @@ namespace Cheatscape
         public static void SelectCheat()
         {
             for (int i = 0; i < AllAnswers.Count; i++)
-            {     
-                
-                if (Rules_List.AccessCurrentRule != Rules_List.GetList().Length)
+            {
+                if (AllAnswers[i].Item1.myRule.X == Rules_List.AccessCurrentRuleList &&
+                    AllAnswers[i].Item1.myRule.Y == Rules_List.AccessCurrentRule &&
+                    AllAnswers[i].Item2 == CurrentSlide)
                 {
-                    if (rating / 2 > 400)
-                    {
-                        rating /= 2;
-                    }
-                    else if (rating - 400 > 0)
-                    {
-                        rating -= 400;
-                    }
-                    else
-                        rating = 0;
+                    isOnTransitionScreen = true;
+                    currentHint = -1;
+                    unlockedHints = -1;
+                    displayingHint = false;
+                    rating += 100;
+                    Hint_File_Manager.LoadHints();
+                    Pause_Menu.gameIsPaused = false;
+                    Level_Transition.LoadSpecialRule();
+                    CurrentLevel++;
+                    File_Manager.LoadLevel();
                 }
+
             }
+            if (Rules_List.AccessCurrentRule != Rules_List.GetList().Length && !isOnTransitionScreen && !End_Screen.AccessCleared)
+            {
+
+                if (rating / 2 > 400)
+                {
+                    rating /= 2;
+                }
+                else if (rating - 400 > 0)
+                {
+                    rating -= 400;
+                }
+                else
+                    rating = 0;
+            }      
             FindingCheat = false;
         }
 
